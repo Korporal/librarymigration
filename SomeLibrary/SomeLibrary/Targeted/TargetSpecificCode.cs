29bcbcef5e60f5cb88f25d3d484c5c8db345d749
Korporal <hugh_gleaves@hotmail.com>
2021-09-13 10:06:47 -0700

    Add some code that was part of the original NetF library but is not available for NetS and could not be built for NetS. We add the code and the two implementations that we need, one for NetF and the other for NetC.

---
+++ SomeLibrary/SomeLibrary/Targeted/TargetSpecificCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SomeLibrary.Targeted
+{
+    // This is code that must be implemented differently between NetF and NetC likley using class and methods that are 
+    // only available on these targets, stuff not available for NetS. The exampole code here is trivial for ease of readability.
+    public class TargetSpecificCode
+    {
+#if NET472
+        public const string Target = "NetF";
+#endif
+
+#if NET5_0
+        public const string Target = "NetS";
+#endif
+    }
+}
