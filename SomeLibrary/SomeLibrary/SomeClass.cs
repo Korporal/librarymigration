df50773d7570540152a00366f9639baf44351dd2
Korporal <hugh_gleaves@hotmail.com>
2021-09-13 09:58:33 -0700

    Create a new .NetS class library project, in there we have all the code from the original that is buildable against .NetS.

---
+++ SomeLibrary/SomeLibrary/SomeClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SomeLibrary
+{
+    // This code is all buildable against .NetS 2.0
+    public class SomeClass
+    {
+        public SomeClass()
+        {
+
+        }
+    }
+}
